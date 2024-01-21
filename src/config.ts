613d03c786865fbf3a63aabb3185e24a460d05c2
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-01-21 13:50:21 -0300

    link api em producao

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl: 'https://localhost:7017',
+  apiUrl: 'http://ninemed.somee.com/',
   authRoles: {
     sa: ['Administrador'], 
     medico: ['Administrador', 'ProfissionalSaude'], 
