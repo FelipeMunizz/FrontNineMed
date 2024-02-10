6a0eff3a0eedf030cf73e04f139b6c0acdfa35b6
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-02-10 16:37:43 -0300

    NMB-54 | Criação do Model Paciente

---
+++ src/app/shared/models/paciente.model.ts
@@ -0,0 +1,86 @@
+export class AdicionarPaciente {
+    idClinica: number;
+    nome: string;
+    dataNascimento: string;
+    estadoCivil: number;
+    rg: string;
+    cpf: string;
+    profissao: string;
+
+    logradouro: string;
+    numero: string;
+    complemento: string;
+    bairro: string;
+    cep: string;
+    estado: number;
+    cidade: string;
+    codMunicipio: string;
+
+    nomeContato: string;
+    numeroContato: string;
+    tipoContato: number;
+    email: string;
+    horarioComercial: boolean = false;
+    lembretes: boolean = false;
+
+    numeroCarterinha: string;
+    validade: string;
+    contratoPlano: string;
+    observacoes: string;
+    idConvenio: number;
+
+    nomeFamiliar: string;
+    grauParentesco: string;
+    telefoneFamiliar: string;
+}
+
+export class Paciente {
+    id: number;
+    idClinica: number;
+    nome: string;
+    dataNascimento: string;
+    estadoCivil: number;
+    rg: string;
+    cpf: string;
+    profissao: string;
+}
+
+export class ContatoPaciente {
+    id: number;
+    nome: string;
+    numeroContato: string;
+    email: string;
+    tipoContato: number;
+    horarioComercial: boolean;
+    lembretes: boolean;
+    idClinica: number;
+}
+
+export class EnderecoPaciente {
+    id: number
+    logradouro: string;
+    numero: string;
+    complemento: string;
+    bairro: string;
+    cep: string;
+    estado: number;
+    cidade: string;
+    codMunicipio: string;
+    idClinica: number;
+}
+
+export class ConvenioPaciente{
+    id: number;
+    numeroCarterinha: string;
+    validade: string;
+    contratoPlano: string;
+    observacoes: string;
+    idConvenio: number;
+}
+
+export class FamiliarPaciente{
+    id: number;
+    nomeFamiliar: string;
+    grauParentesco: string;
+    telefoneFamiliar: string;
+}
