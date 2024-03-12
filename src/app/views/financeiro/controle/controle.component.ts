d2b04a2291dad5040c530b2aa664bb07b11ecb93
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-03-12 20:18:26 -0300

    NMB-72 | Chamada da ContaBancaria padrão

--- src/app/views/financeiro/controle/controle.component.ts
+++ src/app/views/financeiro/controle/controle.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AppLoaderService } from 'app/shared/services/app-loader/app-loader.service';
 import { LancamentoService } from 'app/shared/services/app-models/lancamento.service';
 import { JwtAuthService } from 'app/shared/services/auth/jwt-auth.service';
+import { ContaBancariaService } from 'app/shared/services/app-models/conta-bancaria.service';
 
 @Component({
   selector: 'app-controle',
@@ -13,20 +14,27 @@ export class ControleComponent implements OnInit {
 
   color: string;
   valorSadoGeral: number = 0;
+  contaBancaria: string = '';
 
   constructor(
     private router: Router,
     private atuhService: JwtAuthService,
     private loader: AppLoaderService,
-    private lancamentoService: LancamentoService
+    private lancamentoService: LancamentoService,
+    private contaBancariaService: ContaBancariaService
   ) { }
 
   ngOnInit(): void {
-    this.SaldoGeral();
+    this.LoadDadosIniciais();
   }
 
   TelaCadastro(path: string){
     this.router.navigateByUrl(`financeiro/${path}`)
+  }
+
+  LoadDadosIniciais(){
+    this.SaldoGeral();
+    this.ContaBancariaPadrao();
   }
 
   SaldoGeral(){
@@ -42,6 +50,16 @@ export class ControleComponent implements OnInit {
       else
         this.color = 'danger';
     });
+  }
+
+  ContaBancariaPadrao(){
+    //Ajustar o parametro apos criar as telas de cadastro e configuracao, 
+    //o idContaBancaria deve ser o cadastrado nas configurações do financeiro
+    
+    this.contaBancariaService.ObterContaBancaria(1)
+    .subscribe((conta) => {
+      this.contaBancaria = conta.nome
+    })
   }
 
 }
