e20c9fdce5c7cb29a8d483de7eaf60debe0616e1
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-03-15 13:32:49 -0300

    NMB-76 | Tela Configuracao Financeira

---
+++ src/app/shared/models/configuracao-financeira.model.ts
@@ -0,0 +1,8 @@
+export class ConfiguracaoFinanceira{
+    id: number;
+    idBanco: number;
+    idContaBancaria: number;
+    idCentroCusto: number;
+    idFormaPagamento: number;
+    idClinica: number;
+}
