7c66ba233e70bdefc0e060dc812ad3aa67930718
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-04-04 22:42:47 -0300

    NMB-86 | Novas Implementações

---
+++ src/app/views/dashboard/analytics/chamada-senha-modal/chamada-senha-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chamada-senha-modal',
+  templateUrl: './chamada-senha-modal.component.html',
+  styleUrls: ['./chamada-senha-modal.component.scss']
+})
+export class ChamadaSenhaModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -1,23 +1,61 @@
-import { SharedMaterialModule } from "app/shared/shared-material.module";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { NgChartsModule } from "ng2-charts";
-import { SharedPipesModule } from "app/shared/pipes/shared-pipes.module";
 import { DashboardRoutes } from "./dashboard.routing";
 import { AnalyticsComponent } from "./analytics/analytics.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatTableModule } from "@angular/material/table";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { FileUploadModule } from "ng2-file-upload";
+import { NgxMaskModule } from "ngx-mask";
+import { QuillModule } from "ngx-quill";
+import { ChamadaSenhaModalComponent } from './analytics/chamada-senha-modal/chamada-senha-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatListModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatProgressBarModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatStepperModule,
     FlexLayoutModule,
-    NgChartsModule,
-    SharedPipesModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    MatTableModule,
+    QuillModule.forRoot(),
+    FileUploadModule,
+    MatTooltipModule,
+    MatDialogModule,
+    NgxMaskModule.forRoot(),
+    MatSelectModule,
     RouterModule.forChild(DashboardRoutes)
   ],
-  declarations: [AnalyticsComponent],
+  declarations: [AnalyticsComponent, ChamadaSenhaModalComponent],
   exports: []
 })
 export class DashboardModule {}
